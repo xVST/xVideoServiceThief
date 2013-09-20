5156fb83402ff0b9835f99495157fbf629843c98
xVST <xeskuu.xvst@gmail.com>
2013-09-20 19:25:10 +0200

    Qt 5.1 compilation fix

--- src/qtsingleapplication/src/qtlocalpeer.cpp
+++ src/qtsingleapplication/src/qtlocalpeer.cpp
@@ -50,6 +50,8 @@ static PProcessIdToSessionId pProcessIdToSessionId = 0;
 #endif
 #if defined(Q_OS_UNIX)
 #include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
 #endif
 
 namespace QtLP_Private {
@@ -93,7 +95,7 @@ QtLocalPeer::QtLocalPeer(QObject* parent, const QString &appId)
         socketName += QLatin1Char('-') + QString::number(sessionId, 16);
     }
 #else
-    socketName += QLatin1Char('-') + QString::number(QtLP_Private::getuid(), 16);
+	socketName += QLatin1Char('-') + QString::number(::getuid(), 16);
 #endif
 
     server = new QLocalServer(this);
