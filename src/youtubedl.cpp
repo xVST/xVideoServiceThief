357bf4abb3b4f490dd49efd6f5e70ca7dc08c000
xVST <xeskuu.xvst@gmail.com>
2017-11-27 18:54:56 +0100

    Implemented the Youtube-DL wrapper

--- src/youtubedl.cpp
+++ src/youtubedl.cpp
@@ -1,4 +1,36 @@
+/*
+*
+* This file is part of xVideoServiceThief,
+* an open-source cross-platform Video service download
+*
+* Copyright (C) 2007 - 2017 Xesc & Technology
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with xVideoServiceThief. If not, see <http://www.gnu.org/licenses/>.
+*
+* Contact e-mail: Xesc <xeskuu.xvst@gmail.com>
+* Program URL   : http://www.xvideothief.com/
+*
+*/
+
 #include "youtubedl.h"
+
+#include "http.h"
+#include "tools.h"
+
+#ifndef Q_OS_WIN32
+#include <signal.h>
+#endif
 
 YoutubeDL::YoutubeDL(QString appPath, QString workingDir)
 {
@@ -10,10 +42,10 @@ YoutubeDL::YoutubeDL(QString appPath, QString workingDir)
 	// create a new qprocess
 	process = new QProcess();
 	// connect signals
-//	connect(process, SIGNAL(started()), this, SLOT(started()));
-//	connect(process, SIGNAL(finished(int, QProcess::ExitStatus)), this, SLOT(finished(int, QProcess::ExitStatus)));
-//	connect(process, SIGNAL(readyReadStandardOutput()), this, SLOT(readyReadStandardOutput()));
-//	connect(process, SIGNAL(readyReadStandardError()), this, SLOT(readyReadStandardError()));
+	connect(process, SIGNAL(started()), this, SLOT(started()));
+	connect(process, SIGNAL(finished(int, QProcess::ExitStatus)), this, SLOT(finished(int, QProcess::ExitStatus)));
+	connect(process, SIGNAL(readyReadStandardOutput()), this, SLOT(readyReadStandardOutput()));
+	connect(process, SIGNAL(readyReadStandardError()), this, SLOT(readyReadStandardError()));
 	// cofnigure process
 	process->setWorkingDirectory(workingDir);
 }
@@ -25,15 +57,253 @@ YoutubeDL::~YoutubeDL()
 
 void YoutubeDL::init()
 {
+	stopReason = EnumYoutubeDL::NO_STOPPED;
+	resuming = false;
+	errorCode = 0;
+	fileSize = 0;
+	downloadSpeed = 0;
+	timeRemaining = 0;
+}
+
+bool YoutubeDL::isYoutubeDLappInstalled()
+{
+	return QFile::exists(appPath);
+}
+
+int YoutubeDL::download(const QString URL, QString destination, QString fileName, QStringList params, bool autoName)
+{
+	if ( ! isYoutubeDLappInstalled())
+		return EnumYoutubeDL::YOUTUBE_DL_APP_MISSING;
+
+	// clean destination dir
+	destination = QDir::cleanPath(destination);
+
+#ifdef Q_OS_WIN32
+	// fix Qt4.4.0 bug in windows
+	if (destination.indexOf(":/") == -1)
+		destination.replace(":", ":/");
+#endif
+
+	// check if is already downloading another file
+	if (isDownloading())
+		return EnumHTTP::ALREADY_DOWNLOADING;
+
+	// create the destination path, if it don't exists
+	if ( ! QDir(destination).exists())
+		if ( ! QDir(destination).mkpath(destination))
+			return EnumHTTP::UNABLE_CREATE_DIR;
+
+	// set a default name (only if it is empty)
+	if (fileName.isEmpty())
+		fileName = "download.file";
+
+	fileName = QFileInfo(fileName).fileName();
+
+	// set destination file name
+	fileName = cleanFileName(fileName);
+
+	// get an unique file name for this download
+	if (autoName)
+		fileName = uniqueFileName(destination + "/" + fileName);
+	else
+		fileName = destination + "/" + fileName;
+
+	// set file info
+	destinationFile = QFileInfo(fileName);
+
+	// set as downloading
+	resuming = false;
+
+	// build the command line
+	QStringList commandLine = QStringList() << "-o" << fileName << URL;
+	foreach (QString value, params)
+	{
+		commandLine << value.split("=", QString::SkipEmptyParts);
+	}
+
+	// start download
+	process->start(appPath, commandLine);
+
+	// ok
+	return EnumYoutubeDL::NO_YOUTUBE_DL_ERROR;
+}
+
+int YoutubeDL::resume(const QString URL, QString fileName)
+{
+	if ( ! isYoutubeDLappInstalled())
+		return EnumYoutubeDL::YOUTUBE_DL_APP_MISSING;
+
+	// check if is already downloading another file
+	if (isDownloading())
+		return EnumHTTP::ALREADY_DOWNLOADING;
+
+	if ( ! QFile::exists(fileName + ".part"))
+		return EnumHTTP::MISSING_RESUME_FILE;
+
+	// set file info
+	destinationFile = QFileInfo(fileName);
+
+	// set as resuming
+	resuming = true;
+
+	// resume download
+	process->start(appPath, QStringList() << "-o" << fileName << URL);
+
+	// ok
+	return EnumYoutubeDL::NO_YOUTUBE_DL_ERROR;
+}
+
+void YoutubeDL::pause()
+{
+	if (isDownloading())
+	{
+		stopReason = EnumYoutubeDL::USER_PAUSED;
+		// kill the rtmp process
+		process->kill();
+	}
+}
+
+void YoutubeDL::cancel()
+{
+	if (isDownloading())
+	{
+		stopReason = EnumYoutubeDL::USER_CANCELLED;
+		// kill the rtmp process
+		process->kill();
+		// remove file from disc
+		QFile::remove(destinationFile.absoluteFilePath() + ".part");
+	}
+}
+
+void YoutubeDL::pauseOnDestroy(bool pauseOnDestroyF)
+{
+	this->pauseOnDestroyF = pauseOnDestroyF;
+}
+
+bool YoutubeDL::isDownloading()
+{
+	return process->state() != QProcess::NotRunning;
+}
+
+int YoutubeDL::getFileSize()
+{
+	return fileSize;
+}
+
+int YoutubeDL::getDownloadSpeed()
+{
+	return downloadSpeed;
+}
+
+int YoutubeDL::getTimeRemaining()
+{
+	return timeRemaining;
+}
+
+QFileInfo YoutubeDL::getDestinationFile()
+{
+	return destinationFile;
 }
 
 QJsonDocument YoutubeDL::getVideoInformation(const QString URL)
 {
     QStringList arguments = QStringList() << "-j" << URL;
     // execute command
-    process->start(appPath, arguments);
-    process->waitForFinished();
-    QString output(process->readAllStandardOutput());
+    QProcess process;
+    process.start(appPath, arguments);
+    process.waitForFinished();
+    QString output(process.readAllStandardOutput());
     // parse the json
     return QJsonDocument::fromJson(output.toUtf8());
+}
+
+void YoutubeDL::started()
+{
+	init();
+	// start timer
+//	singleTimerShot();
+	// send download started signal
+	if (resuming)
+		emit downloadResumed();
+	else // not resuming (starting)
+		emit downloadStarted();
+}
+
+void YoutubeDL::finished(int /*exitCode*/, QProcess::ExitStatus /*exitStatus*/)
+{
+	// user cancelled
+	if (stopReason == EnumYoutubeDL::USER_CANCELLED)
+		emit downloadCanceled();
+	// user paused
+	else if (stopReason == EnumYoutubeDL::USER_PAUSED)
+		emit downloadPaused(destinationFile);
+	// download error
+	else if (stopReason == EnumYoutubeDL::DOWNLOAD_ERROR)
+		emit downloadError(errorCode);
+	// download finished
+	else if (stopReason == EnumYoutubeDL::NO_STOPPED)
+		emit downloadFinished(destinationFile);
+}
+
+void YoutubeDL::readyReadStandardOutput()
+{
+	parseOutput(QString::fromLocal8Bit(process->readAllStandardOutput()));
+}
+
+void YoutubeDL::readyReadStandardError()
+{
+	parseOutput(QString::fromLocal8Bit(process->readAllStandardError()));
+}
+
+void YoutubeDL::parseOutput(QString output)
+{
+	output = output.trimmed();
+	// split output into lines
+	QStringList lines = output.replace("\r", "\n").split("\n");
+	// parse each line
+	foreach(QString line, lines)
+	{
+		qDebug() << line;
+
+		QRegExp downloadRegExp("\\[download\\]\\s+(.+)%\\s+of\\s+(.+)\\s+at\\s+(.+)\\s+ETA\\s+(.+)");
+		// parese information
+		if (downloadRegExp.indexIn(line) > -1)
+		{
+			// get file size
+			fileSize = fileSizeToBytes(downloadRegExp.capturedTexts().at(2));
+			// parse the download speed
+			if (downloadRegExp.capturedTexts().at(3) != "Unknown speed")
+			{
+				QString speed = downloadRegExp.capturedTexts().at(3);
+				// convert download speed to bytes
+				downloadSpeed = fileSizeToBytes(speed.replace("/s", ""));
+			}
+			// parse the ETA time
+			if (downloadRegExp.capturedTexts().at(4) != "Unknown speed")
+			{
+				QString dhms = downloadRegExp.capturedTexts().at(4);
+				QStringList parts = dhms.split(":");
+				// parse inits
+				int index = parts.count() - 1;
+				timeRemaining = 0;
+				// calcule the remaining time
+				foreach (QString value, parts)
+				{
+					timeRemaining += value.toInt()*pow(60, index);
+					index--;
+				}
+			}
+			// calcule downlaod progress
+			QString progress = downloadRegExp.capturedTexts().at(1);
+			// send the download event
+			emit downloadEvent(progress.toFloat()*100, 10000);
+		}
+
+		QRegExp errorRegExp("ERROR:");
+		// parse error
+		if (errorRegExp.indexIn(line) > -1)
+		{
+			stopReason = EnumYoutubeDL::DOWNLOAD_ERROR;
+		}
+	}
 }
--- src/youtubedl.h
+++ src/youtubedl.h
@@ -1,3 +1,28 @@
+/*
+*
+* This file is part of xVideoServiceThief,
+* an open-source cross-platform Video service download
+*
+* Copyright (C) 2007 - 2017 Xesc & Technology
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with xVideoServiceThief. If not, see <http://www.gnu.org/licenses/>.
+*
+* Contact e-mail: Xesc <xeskuu.xvst@gmail.com>
+* Program URL   : http://www.xvideothief.com/
+*
+*/
+
 #ifndef YOUTUBEDL_H
 #define YOUTUBEDL_H
 
@@ -9,22 +34,87 @@ static const QString YOUTUBE_DL_APP_PATH = "youtube-dl.exe"; //!< Youtube-dl app
 static const QString YOUTUBE_DL_APP_PATH = "youtube-dl"; //!< Youtube-dl app (*unix)
 #endif
 
+namespace EnumYoutubeDL
+{
+	enum Error
+	{
+		NO_YOUTUBE_DL_ERROR = 0,		//0
+		YOUTUBE_DL_APP_MISSING = 100,	//100
+		FAILED_TO_OPEN_FILE,			//101
+		FAILED_TO_CONNECT,				//102
+		COULDNT_RESUME_FLV,				//103
+		DOWNLOAD_INCOMPLETE,			//104
+		PTHREAD_CREATE_FAILED,			//105
+		FILE_NOT_FOUND					//106
+	};
+
+	enum StopReason
+	{
+		NO_STOPPED = 200,			//200
+		DOWNLOAD_FINISHED,			//201
+		USER_CANCELLED,				//202
+		USER_PAUSED,				//203
+		DOWNLOAD_ERROR				//204
+	};
+}
+
 /*! YoutubeDL Qt5 wrapper */
 class YoutubeDL : public QObject
 {
 Q_OBJECT
     private:
-        QProcess *process;				//!< YoutubeDL process
-        QString appPath;				//!< Base dir where Youtube-dl is located (ie: /users/xesk/.xvst/)
+        QProcess *process;                      //!< YoutubeDL process
+        QString appPath;                        //!< Base dir where Youtube-dl is located (ie: /users/xesk/.xvst/)
+        QFileInfo destinationFile;              //!< Destination file information
+        EnumYoutubeDL::StopReason stopReason;	//!< Flag for know if the user aborted
+        bool resuming;                          //!< Flag for know if is being resumed
+        int fileSize;                       	//!< File size to download (bytes)
+        int downloadSpeed;                      //!< Current download speed (bytes second)
+        bool pauseOnDestroyF;               	//!< Should pause the download instead of cancel it?
+        int timeRemaining;                      //!< Time remaining in seconds
+        int errorCode;                          //!< Last error code detected
         /*! Init internal variables */
         void init();
+        /*! Parse the current output text */
+        void parseOutput(QString output);
     public:
         /*! Class constructor */
         YoutubeDL(QString appPath, QString workingDir);
         /*! Class destructor */
         ~YoutubeDL();
+        /*! Return if Youtube-DL is installed (detected) */
+        bool isYoutubeDLappInstalled();
+        /*! Start a new asynchronously download */
+        int download(const QString URL, QString destination, QString fileName = "", QStringList params = QStringList(), bool autoName = true);
+        /*! Resume a previous asynchronously download */
+        int resume(const QString URL, QString fileName);
+        /*! Pause the current asynchronously download */
+        void pause();
+        /*! Cancel current download */
+        void cancel();
+        /*! Pause on destroy the Youtube-DL class (only if is downloading) */
+        void pauseOnDestroy(bool pauseOnDestroyF = true);
+        /*! Get if is downloading */
+        bool isDownloading();
+        /*! Get file size */
+        int getFileSize();
+        /*! Get download speed */
+        int getDownloadSpeed();
+        /*! Get time remaining */
+        int getTimeRemaining();
+        /*! Get the destination file name */
+        QFileInfo getDestinationFile();
         /*! Get information  */
         QJsonDocument getVideoInformation(const QString URL);
+    private slots:
+        /*! Rtmpdump started */
+        void started();
+        /*! Rtmpdump finished */
+        void finished(int exitCode, QProcess::ExitStatus exitStatus);
+        /*! output recived */
+        void readyReadStandardOutput();
+        /*! output by error recived */
+        void readyReadStandardError();
     signals:
         /*! when a download started */
         void downloadStarted();
@@ -38,7 +128,7 @@ Q_OBJECT
         void downloadCanceled();
         /*! an error ocurred during the download process */
         void downloadError(int error);
-        /*! when the rtmp read data */
+        /*! when the youtube-dl read data */
         void downloadEvent(int pos, int max);
 };
 
